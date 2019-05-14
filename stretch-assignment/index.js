862855bb76d7eb54c04a6d2f9fe5e708f31b0ec4
Chris Pelayo <chris.e.pelayo@gmail.com>
2019-05-14 15:24:01 -0700

    completed stretch assignment stretch

--- stretch-assignment/index.js
+++ stretch-assignment/index.js
@@ -1,13 +1,18 @@
 const rockets = document.querySelectorAll('.block');
-let orderCount = -1;
+// let orderCount = -1;
 
 for(let rocket of rockets) {
     rocket.setAttribute('src', 'rocket.jpg');
 
-    rocket.addEventListener('click', (event) => {
-        TweenMax.to(rocket, 3, {left: innerWidth - 170});
-        console.log(rocket, event);
-        rocket.style.order = orderCount;
-        orderCount--;
+    rocket.addEventListener('mousedown', (event) => {
+        rocket.style.transform = 'scaleX(1)';
+        TweenMax.to(rocket, 2, {left: innerWidth - 180});
+        // rocket.style.order = orderCount;
+        // orderCount--;
+    })
+
+    rocket.addEventListener('mouseup', (event) => {
+        rocket.style.transform = 'scaleX(-1)';
+        TweenMax.to(rocket, 2, {left: 0});
     })
 }

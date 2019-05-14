435672cbc9981a385045de8c63c6d4ecc530176f
Chris Pelayo <chris.e.pelayo@gmail.com>
2019-05-14 11:00:27 -0700

    added mouseover event for logo

--- js/index.js
+++ js/index.js
@@ -1,1 +1,16 @@
-// Your code goes here
+// This is changing the color of the 'Fun Bush' logo h1 on mouseover.
+const colorArray = ['red', 'orange', 'yellow', 'green', 'blue', 'purple'];
+let count = -1;
+
+const logoHeading = document.querySelector('.logo-heading');
+logoHeading.addEventListener('mouseover', (event) => {
+    if(count < colorArray.length-1) {
+        count++;
+        console.log(count)
+        logoHeading.style.color = colorArray[count];
+    } else {
+        count = -1;
+        logoHeading.style.color = colorArray[count];
+    }
+})
+
